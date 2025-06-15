6293924e7f99803f3d4f7563e7d1b45d60985a64
Yashkumar Gautam <yk.gautam97@gmail.com>
2025-06-15 16:32:55 +0530

    firstcommit

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,14 @@
+import App from "./App";
+
+
+function Chai(){
+    return(
+        <>
+      
+        <h2> basic Chai</h2>
+
+        </>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,8 @@
+function Chai() {
+    return (
+        <h3>chai | vite</h3>
+    )
+    
+}
+
+export default Chai
